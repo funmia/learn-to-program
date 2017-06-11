275d847452ca8c53d9a836c01c7a3219d9beb033
funmi_a <funmiadewodu@yahoo.com>
2017-06-11 01:18:29 +0100

    Added chapter5

---
+++ chapter5/angry-boss.rb
@@ -0,0 +1,4 @@
+puts "Hello Boss"
+puts "What do you want!!!"
+request = gets.chomp
+puts "WHADDAYA MEAN " + request + "?!?  YOU'RE FIRED!!"
---
+++ chapter5/table-of-contents.rb
@@ -0,0 +1,5 @@
+lineWidth = 30
+puts "Table of Contents".center(lineWidth*2)
+puts ("Chapter 1:  Numbers".ljust(lineWidth)) + ("page 1".rjust(lineWidth))
+puts ("Chapter 2:  Letters".ljust(lineWidth)) + ("page 72".rjust(lineWidth))
+puts ("Chapter 2:  Variables".ljust(lineWidth)) + ("page 118".rjust(lineWidth))
